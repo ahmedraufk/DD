64573bc12621dfcaf1f31efdc1d51bfdd82f7398
aklasra3 <ahmedraufk@gatech.edu>
2021-06-26 21:45:00 +0500

    Add game screen

--- src/main/InitialConfigurationScreen.java
+++ src/main/InitialConfigurationScreen.java
@@ -2,16 +2,24 @@ package main;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.geometry.Pos;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.control.Alert;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class InitialConfigurationScreen extends Application {
 
@@ -36,9 +44,9 @@ public class InitialConfigurationScreen extends Application {
 
     /**
      * Creates start screen.
-     * @param stage the stage passed in.
+     * @param primaryStage the stage passed in.
      */
-    private void startScreen(Stage stage) {
+    private void startScreen(Stage primaryStage) {
 
         //creating the starting screen.
         VBox box = new VBox(75);
@@ -104,12 +112,14 @@ public class InitialConfigurationScreen extends Application {
 
                 weaponType = weapon.getValue();
 
-                stage.setScene(game);
+                gameScreen(primaryStage);
+
+                primaryStage.setScene(game);
 
             }
         });
     }
-    private void introScreen(Stage stage){
+    private void introScreen(Stage primaryStage){
 
         //Create the intro Screen
         VBox box = new VBox(75);
@@ -127,15 +137,87 @@ public class InitialConfigurationScreen extends Application {
 
         //Move to start screen
         introBtn.setOnMouseClicked(e -> {
-            startScreen(stage);
-            stage.setScene(start);
+            startScreen(primaryStage);
+            primaryStage.setScene(start);
         });
 
 
 
 
     }
-    private void gameScreen(Stage stage){
+    private void gameScreen(Stage stage)  {
+        //Main screen of the game
+        BorderPane screen = new BorderPane();
+        // Top UI for the game we can see for money
+        HBox UI = new HBox();
+        int moneyAmount = 0;
+        if(level == Difficulty.EASY){
+            moneyAmount = 1000;
+        } else if ((level == Difficulty.MEDIUM)){
+            moneyAmount = 500;
+        }
+
+
+        Text money = new Text("Money:" + moneyAmount);
+        UI.getChildren().addAll(money);
+
+        //Player Creation
+        StackPane playerScreen = new StackPane();
+        playerScreen.setAlignment(Pos.CENTER);
+
+
+
+
+
+
+
+        Rectangle player = new Rectangle(50,50);
+        playerScreen.getChildren().addAll(player);
+
+        //Create Exit Left
+        VBox left = new VBox();
+        Rectangle exit1 = new Rectangle(25,100);
+        exit1.setFill(Color.GREEN);
+        exit1.setStroke(Color.GREEN);
+
+        left.getChildren().addAll(exit1);
+        left.setAlignment(Pos.CENTER);
+
+        //Create Exit Right
+        VBox right = new VBox();
+        Rectangle exit2 = new Rectangle(25,100);
+        exit2.setFill(Color.GREEN);
+        exit2.setStroke(Color.GREEN);
+
+        right.getChildren().addAll(exit2);
+        right.setAlignment(Pos.CENTER);
+
+        //Create Exit Bottom
+        HBox bottom = new HBox();
+        Rectangle exit3 = new Rectangle(100,25);
+        exit3.setFill(Color.GREEN);
+        exit3.setStroke(Color.GREEN);
+
+        Rectangle exit4 = new Rectangle(100,25);
+        exit4.setFill(Color.GREEN);
+        exit4.setStroke(Color.GREEN);
+
+        bottom.setSpacing(50);
+        bottom.getChildren().addAll(exit3,exit4);
+        bottom.setAlignment(Pos.CENTER);
+
+
+
+
+        screen.setTop(UI);
+        screen.setCenter(playerScreen);
+        screen.setLeft(left);
+        screen.setRight(right);
+        screen.setBottom(bottom);
+
+        //Create scene
+        game = new Scene(screen,525,525);
+
 
     }
 

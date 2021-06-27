f4150fc4c71d611e7a2edd52926462cceb2aa9ae
nathanael3 <nathanael3@gatech.edu>
2021-06-27 17:30:29 -0500

    Add JUnit test for navigation and for checking empty name

--- src/Test/M2Test.java
+++ src/Test/M2Test.java
@@ -1,9 +1,10 @@
-package main;
+package Test;
 
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.matcher.control.TextMatchers.hasText;
 
 import javafx.stage.Stage;
+import main.InitialConfigurationScreen;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 import org.testfx.matcher.base.WindowMatchers;

a84918ea7569965611d86ec90c64961048cdfa93
aklasra3 <ahmedraufk@gatech.edu>
2021-06-28 03:43:40 +0500

    add test check difficulty

---
+++ src/Test/DifficultyTest.java
@@ -0,0 +1,46 @@
+
+
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+import main.InitialConfigurationScreen;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
+import static org.testfx.api.FxAssert.verifyThat;
+
+public class DifficultyTest extends ApplicationTest {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        InitialConfigurationScreen cScreen = new InitialConfigurationScreen();
+        cScreen.start(primaryStage);
+    }
+    @Test
+    public void selectDifficultyEasy() {
+        clickOn("Start");
+        clickOn("Select Difficulty");
+        clickOn("EASY");
+        clickOn("Select Weapon");
+        clickOn("AXE");
+        clickOn("Name:");
+        type(KeyCode.A);
+        clickOn("Start!");
+        verifyThat("Money:1000",NodeMatchers.isNotNull());
+
+    }
+
+    @Test
+    public void selectDifficultyHard() {
+        clickOn("Start");
+        clickOn("Select Difficulty");
+        clickOn("HARD");
+        clickOn("Select Weapon");
+        clickOn("AXE");
+        clickOn("Name:");
+        type(KeyCode.A);
+        clickOn("Start!");
+        verifyThat("Money:0",NodeMatchers.isNotNull());
+
+    }
+
+
+}

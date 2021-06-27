dce7757abc90d31decff4af0f21e2760ad14b3af
“bzhang420” <bzhang420@gatech.edu>
2021-06-27 14:31:20 -0400

    edit configuration screen, add 2 test and a text file name readMe explaining my tests.

--- src/Test/ConfigTest.java
+++ src/Test/ConfigTest.java
@@ -1,4 +1,30 @@
 package Test;
 
-public class ConfigTest {
+import javafx.stage.Stage;
+import main.InitialConfigurationScreen;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
+import static org.testfx.api.FxAssert.verifyThat;
+
+public class ConfigTest extends ApplicationTest {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        InitialConfigurationScreen cScreen = new InitialConfigurationScreen();
+        cScreen.start(primaryStage);
+    }
+    @Test
+    public void findWeaponSelection() {
+        clickOn("Start");
+        verifyThat("Select Weapon", NodeMatchers.isNotNull());
+
+    }
+
+    @Test
+    public void findDifficultySelection() {
+        clickOn("Start");
+        verifyThat("Select Difficulty", NodeMatchers.isNotNull());
+
+    }
+
 }
